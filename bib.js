81c5289e14d40fc2c8bbe5ea94a0b2d8c66f241f
Ron Metzger <ron_marvin@yahoo.de>
2022-08-04 10:53:16 +0200

    First Commit

---
+++ bib.js
@@ -0,0 +1,77 @@
+function TextBib(Elem) {
+    switch (Elem) {
+        case "Brainstorming":
+            return "Beim Brainstorming sammeln wir schnell eine Vielzahl von Ideen. Dabei gibt es keine schlechten Ideen, da es hier zunächst nur um die reine Ideensammlung geht und nicht um die Bewertung. Die Ideen werden dabei auf Papier oder digital festgehalten.";
+            break;
+        case "Case Study":
+            return "Case Study nutzen wir für die Beschreibung eines typischen Einzelfalls aus der Praxis. Dabei liegt oft ein Problem vor, worauf eine Lösung und ein Ergebnis folgen. Durch die Analyse des Einzelfalls können wir allgemeingültige Regeln abgeleiten und zukünftig angewenden.";
+            break;
+        case "Mind Map":
+            return "Beim Mind Mapping stellen wir Ideen visuell dar und ordnen diese direkt in Themenbereiche. Durch das clustering entsteht eine geordnete Übersicht aller Ideen.";
+            break;
+        case "Offering Map":
+            return "Unter Offering Map verstehen wir die Beschreibung der Inhalte des Service in Form von Wörtern, Bildern oder Illustrationen, um Stakeholdern ein Verständnis der Kernfunktionen des Service zu vermitteln.";
+            break;
+        case "Design Fiction":
+            return "Hierbei verwenden wir ein Designansatz, der durch Prototyping und das Erzählen von Geschichten die Entwicklung unterstützt, über neue Serviceideen zu spekulieren.";
+            break;
+        case "Personas":
+            return "Bei Personas erstellen wir fiktive Charaktere, basierend auf Stakeholder-Recherchen, um verschiedene Benutzertypen darzustellen, welche den Service auf ähnliche Weise nutzen könnten.";
+            break;
+        case "Roleplay":
+            return "Durch ein Rollenspiel zeigen wir den Stakeholdern alle Funktionen, Prozesse und Berührungspunkte eines bereits existierenden Service. Dadurch soll die Kommunikation verbessert und Probleme schneller gelöst werden.";
+            break;
+        case "Storytelling":
+            return "Das Storytelling verwenden wir, um eine Service-Idee in Form einer Geschichte durch den Gebrauch von einfachen Wörtern zu illustrieren. Das Ziel ist es, die Erforschung der finalen Service-Idee zu unterstützen.";
+            break;
+        case "Voice of Customer Analysis":
+            return "Unter Voice of Customer Analysis verstehen wir die Ermittlung ausgesprochener als auch unausgesprochener Stakeholder-Wünsche. Diese werden nach der Ermittlung in quantifizierbare, bewertbare und strukturierte Serviceanforderungen transformiert.";
+            break;
+        case "Concept Map":
+            return "Concept Map verwenden wir, um Stakeholder co-kreativ in frühen Phasen miteinzubeziehen. Alltägliche Service-Erlebnisse der Stakeholder werden dadurch ins Design miteinfließen, um somit eine optimale Lösung zu entwickeln. Die Visualisierung von Begriffen und Zusammenhängen erfolgt in Form eines Netztes. Durch die graphische Darstellung von Informationen können Gedanken dazu besser geordnet werden.";
+            break;
+        case "Cross Channel Views":
+            return "Cross Channel Views verwenden wir, um alle Stakeholder-Berührungspunkte im Serviceprozess darzustellen, sodass skizziert wird, in welchen Phasen die Berührungspunkte auftreten und wie Kunden aus verschiedenen Stakeholder-Perspektiven den Service wahrnehmen.";
+            break;
+        case "Lego Serious Play":
+            return "Beim Lego Serious Play verwenden wir Lego-Steine und -Figuren. Durch ein moderierten Prozess stellen Stakeholder Service-Ideen und -Prozesse mit Lego dar. Ziel ist es, die Kommunikation zu verbessern und die Problemlösung zu beschleunigen.";
+            break;
+        case "Storyboard":
+            return "Das Storyboard verwenden wir um eine zeichnerische Version eines Drehbuchs oder eine Visualisierung eines Service-Konzepts zu erstellen. Dieses beschreibt, wie Stakeholder einen Service erleben ung gibt Auskunft, ob das vorgesehene Service-Design die Stakehoder zielführend unterstützt.";
+            break;
+        case "Understanding Workshop":
+            return "Beim Understanding Workshop verstehen wir eine Sitzung mit Stakeholdern zur Erörterung eines gemeinsamen Verständnisses von Service-Ideen, -Designs und -Konzepten.";
+            break;
+        case "Cognitive Walktrough":
+            return "Beim Cognitive Walkthrough verstehen wir eine kognitive Durchwanderung des Serviceprozesses aus Benutzerperspektive. Durch die Bearbeitung von Aufgaben bewertet der Stakeholder die Benutzerfreundlichkeit des Service.";
+            break;
+        case "Customer Journey Map":
+            return "Hier erstellen wir eine visuelle Darstellung des Kaufprozesses der Kunden. Dabei werden alle Schritte des Kundens abgebildet, sowie alle Berührungspunkte.";
+            break;
+        case "Design Games":
+            return "Bei Design Games erarbeiten wir durch einen spielerischen Prozess Service-Ideen. Dabei werden zu Beginn Spielregeln bzw. Rahmenbedingungen festgelegt.";
+            break;
+        case "Imagine Workshop":
+            return "Beim Imagine Workshop entwerfen wir in Teams Service Szenarien, die auf einem geteilten Verständnis von Kunden basieren. Ziel ist es dabei neue Konzepte für Service Innovationen oder Optimierungen zu entwickeln.";
+            break;
+        case "Video Prototyping":
+            return "Hierbei visualisieren wir Service-Ideen und -Konzepte in Form eines Videos, um beispielsweise Storytelling Ansätze zu testen.";
+            break;
+        case "Rapid Prototyping":
+            return "Beim Rapid Prototyping verwenden wir die Kernaspekte einer Idee und setzen diese anhand eines schnell entwickelten Prototypen um.";
+            break;
+        case "Recommendation Configurator":
+            return "Beim Recommendation Configurator erstellen wir ein Konfigurator, der Aufgrund der Auswahl von Kriterien, Prozesse, u. ä. eine Empfehlung ausgibt.";
+            break;
+        case "Service Blueprint":
+            return "Hierbei visualisieren wir den kompletten Dienstleistungsprozess aller beteiliter Prozessschritte, Ereignissen, Entscheidungen und Stakeholder-Interaktionen.";
+            break;
+        case "Virtual Simulation":
+            return "Unter Virtual Simulation verstehen wir die Simulatoin eines immersiven Service-Erlebnisses innerhalb einer virtuellen Umgebung, um die Service-Idee darzustellen und die Kommunikation virtuell zu unterstützen.";
+            break;
+        case "Short Clips":
+            return "In Form eines kurzen Videos bzw. einer kurzen Animation stellen wir hier die Service-Idee bzw. das Service-Konzept audiovisuell dar. Durch diesen geringen Aufwand ist es uns möglich, mehrere Service-Ideen parallel zu testen.";
+            break;
+    }
+
+}
---
+++ isotope.pkgd.js
@@ -0,0 +1,25 @@
+// store filter for each group
+var filters = {};
+
+$demo.on('click', '.button', function () {
+  var $this = $(this);
+  // get group key
+  var $buttonGroup = $this.parents('.button-group');
+  var filterGroup = $buttonGroup.attr('data-filter-group');
+  // set filter for group
+  filters[filterGroup] = $this.attr('data-filter');
+  // combine filters
+  var filterValue = concatValues(filters);
+  $grid.isotope({ filter: filterValue });
+  document.getElementById("Erfahren").style.height = "auto";
+  document.getElementById("Unerfahren").style.height = "auto";
+});
+
+// flatten object by concatting values
+function concatValues(obj) {
+  var value = '';
+  for (var prop in obj) {
+    value += obj[prop];
+  }
+  return value;
+}
